40c9a6557103586fc9745b05535b776f8d3d75b3
VIKAS JANGID <82387516+Vikas-jangid@users.noreply.github.com>
2021-05-11 10:57:32 +0530

    Extra Lines Removed

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,19 @@
   <v-app>
     <v-main>
       <top-nav></top-nav>
-
      <router-view ></router-view>
     </v-main>
   </v-app>
 </template>
-
 <script>
-
 import Topnav from './components/Topnav.vue'
-
 export default {
   name: "App",
-
   components: {
-
     'top-nav' : Topnav
   },
-
   data: () => ({
-    //
+  //
   }),
 };
 </script>
