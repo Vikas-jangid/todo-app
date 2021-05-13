fb5a1a91c5618af5be30bed47f6657101c333aaa
VIKAS JANGID <82387516+Vikas-jangid@users.noreply.github.com>
2021-05-13 12:52:19 +0530

    Update App.vue
    Extra Lines Are Removed

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,11 @@
     <v-main>
       <top-nav></top-nav>
       <router-view></router-view>
-
-
     </v-main>
   </v-app>
 </template>
 <script>
 import Topnav from "./components/Topnav.vue";
-
-
-
 export default {
   name: "App",
   components: {
